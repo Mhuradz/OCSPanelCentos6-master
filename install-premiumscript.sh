11e9e5cdbe5d46e64de9abd33cf0b6bda85c7bcd
Mhuradz <40193115+Mhuradz@users.noreply.github.com>
2019-06-19 15:40:45 +0800

    Update install-premiumscript.sh

--- install-premiumscript.sh
+++ install-premiumscript.sh
@@ -21,7 +21,7 @@ cat > /root/limit.sh <<END3
 END3
 
 cd /usr/local/bin
-wget -O premium-script.tar.gz "https://raw.githubusercontent.com/Mhuradz143/OCSPanelCentos6/master/premium-script.tar.gz"
+wget -O premium-script.tar.gz "https://raw.githubusercontent.com/Mhuradz/OCSPanelCentos6/master/premium-script.tar.gz"
 tar -xvf premium-script.tar.gz
 rm -f premium-script.tar.gz
 
